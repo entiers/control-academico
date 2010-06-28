f38b6bd8cf417f6e2ad7e9b13dd85206fcfa818d
shakamca <shakamca@0b4acdf5-fa7f-4634-95a9-6fce243d373d>
2010-06-28 12:56:49 +0000

    Se agregó funcionalidad de "Agregar Tipo de Asignacion"

---
+++ src/trabajo-social/src/main/java/gt/edu/usac/trabajosocial/dominio/wrapper/WrapperTipoAsignacion.java
@@ -0,0 +1,108 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package gt.edu.usac.trabajosocial.dominio.wrapper;
+
+import gt.edu.usac.trabajosocial.dominio.TipoAsignacion;
+import javax.validation.constraints.Size;
+import org.hibernate.validator.constraints.NotEmpty;
+
+/**
+ *
+ * Contiene los atributos del Tipo de Asignación que serán ingresados
+ * o actualizado a la BD. El wrapper se utiliza en las páginas de
+ * <code>agregarTipoAsignacion.htm</code> y <code>editarTipoAsignacion.htm</code>.
+ * @author Mario Batres
+ * @version 1.0
+ */
+
+public class WrapperTipoAsignacion {
+//______________________________________________________________________________
+    private String descripcion;
+//______________________________________________________________________________
+    @NotEmpty(message="{validacion.campoObligatorio}")
+    @Size(max = 100, message = "{validacion.caracteresMaximos}")
+    private String nombre;
+//______________________________________________________________________________
+    private boolean habilitado;
+//______________________________________________________________________________
+    /**
+     * Constructor del Tipo de Asignación, se inicializan los atributos a mostrar en las
+     * páginas de <code>agregarTipoAsignacion.htm</code> y <code>editarTipoAsignacion.htm</code>.
+     */
+    public WrapperTipoAsignacion() {
+        this.descripcion = "";
+        this.habilitado = true;
+        this.nombre = "";
+    }
+//______________________________________________________________________________
+    /**
+     * Se agrega al wrapper la informacion del pojo de tipo {@link TipoAsignacion}.
+     *
+     * @param tipoAsignacion Pojo de tipo {@link TipoAsignacion}
+     */
+    public void agregarWrapper(TipoAsignacion tipoAsignacion) {
+        this.descripcion = tipoAsignacion.getDescripcion();
+        this.nombre = tipoAsignacion.getNombre();
+        this.habilitado = tipoAsignacion.isHabilitado();
+    }
+
+//______________________________________________________________________________
+    /**
+     * Se agrega la informacion del pojo de tipo {@link TipoAsignacion} al wrapper.
+     *
+     * @param tipoAsignacion Pojo de tipo {@link TipoAsignacion}
+     */
+    public void quitarWrapper(TipoAsignacion tipoAsignacion) {
+        tipoAsignacion.setDescripcion(descripcion);
+        tipoAsignacion.setNombre(nombre);
+        tipoAsignacion.setHabilitado(habilitado);
+    }
+//______________________________________________________________________________
+
+    /**
+     * @return the descripcion
+     */
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    /**
+     * @param descripcion the descripcion to set
+     */
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+//______________________________________________________________________________
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+//______________________________________________________________________________
+    /**
+     * @return the habilitado
+     */
+    public boolean isHabilitado() {
+        return habilitado;
+    }
+
+    /**
+     * @param habilitado the habilitado to set
+     */
+    public void setHabilitado(boolean habilitado) {
+        this.habilitado = habilitado;
+    }
+
+    
+}
---
+++ src/trabajo-social/src/main/java/gt/edu/usac/trabajosocial/servicio/impl/ServicioTipoAsignacionImpl.java
@@ -0,0 +1,103 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package gt.edu.usac.trabajosocial.servicio.impl;
+
+import gt.edu.usac.trabajosocial.dao.DaoGeneral;
+import gt.edu.usac.trabajosocial.dominio.TipoAsignacion;
+import gt.edu.usac.trabajosocial.servicio.ServicioTipoAsignacion;
+import java.util.List;
+import javax.annotation.Resource;
+import org.hibernate.criterion.DetachedCriteria;
+import org.hibernate.criterion.Restrictions;
+import org.springframework.dao.DataAccessException;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * <p>Contiene la implementación de los metodos que permiten el manejo de la
+ * informacion relacionada con el Tipo Asignacion en la base de datos.</p>
+ *
+ * @author Mario Batres
+ * @version 1.0
+ */
+@Service
+public class ServicioTipoAsignacionImpl implements ServicioTipoAsignacion {
+    /** <p>Permite el acceso y manipulacion a la base de datos.</p> */
+    @Resource
+    private DaoGeneral daoGeneralImpl;
+
+//______________________________________________________________________________
+
+    /**
+     * <p>Este metodo permite agregar la informacion de un tipo de asignación
+     * a la base de datos.</p>
+     *
+     * @param tipoAsignacion Pojo del tipo {@link TipoAsignacion}
+     * @throws DataAccessException Si ocurrio un error de acceso a datos
+     * @throws DataIntegrityViolationException Si ocurrio una violacion de
+     *         de integridad de datos
+     */
+    @Transactional(rollbackFor = Exception.class)
+    @Override
+    public void agregarTipoAsignacion(TipoAsignacion tipoAsignacion) throws DataIntegrityViolationException, DataAccessException {
+        this.daoGeneralImpl.save(tipoAsignacion);
+    }
+//______________________________________________________________________________
+    /**
+     * <p>Este metodo permite actualizar la informacion de un tipo de asignacion
+     * en la base de datos.</p>
+     *
+     * @param tipoAsignacion Pojo del tipo {@link TipoAsignacion}
+     * @throws DataAccessException Si ocurrio un error de acceso a datos
+
+     */
+    @Transactional(rollbackFor = Exception.class)
+    @Override
+    public void actualizarTipoAsignacion(TipoAsignacion tipoAsignacion) throws DataAccessException {
+        this.daoGeneralImpl.save(tipoAsignacion);
+    }
+//______________________________________________________________________________
+    /**
+     * <p>Este metodo permite eliminar fisicamente la informacion de un tipo de asignacion
+     * en la base de datos.</p>
+     *
+     * @param tipoAsignacion Pojo del tipo {@link TipoAsignacion}
+     * @throws DataAccessException Si ocurrio un error de acceso a datos
+
+     */
+    @Transactional(rollbackFor = Exception.class)
+    @Override
+    public void elimiarTipoAsignacion(TipoAsignacion tipoAsignacion) throws DataAccessException {
+        this.daoGeneralImpl.save(tipoAsignacion);
+    }
+//______________________________________________________________________________
+    /**
+     * <p>Este metodo obtiene todos los tipo de asignacion habilitados
+     * en la base de datos.</p>
+     *
+     * @throws DataAccessException Si ocurrio un error de acceso a datos
+     */
+    @Override
+    public List<TipoAsignacion> buscarTipoAsignacion() throws DataAccessException {
+        DetachedCriteria criteria = DetachedCriteria.forClass(TipoAsignacion.class);
+        criteria.add(Restrictions.eq("habilitado", true));
+
+        return this.daoGeneralImpl.find(criteria);
+    }
+    
+//______________________________________________________________________________
+    /**
+     * <p>Este metodo obtiene el tipo de asignacion por su ID</p>
+     *
+     * @throws DataAccessException Si ocurrio un error de acceso a datos
+     */
+    @Override
+    public TipoAsignacion getTipoAsignacion(short idTipoAsignacion) throws DataAccessException {
+        return this.daoGeneralImpl.load(TipoAsignacion.class, idTipoAsignacion);
+    }
+
+}
