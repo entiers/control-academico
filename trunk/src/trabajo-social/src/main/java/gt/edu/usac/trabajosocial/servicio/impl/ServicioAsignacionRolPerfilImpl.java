de09262129b60dd7f2fb3afe5bf0cdb4452c67e3
shakamca <shakamca@0b4acdf5-fa7f-4634-95a9-6fce243d373d>
2010-08-28 16:15:02 +0000

    Agregando funcionalidad de asignacion y desasignación de roles a perfiles.

--- trunk/src/trabajo-social/src/main/java/gt/edu/usac/trabajosocial/servicio/impl/ServicioAsignacionRolPerfilImpl.java
+++ trunk/src/trabajo-social/src/main/java/gt/edu/usac/trabajosocial/servicio/impl/ServicioAsignacionRolPerfilImpl.java
@@ -6,54 +6,81 @@
 package gt.edu.usac.trabajosocial.servicio.impl;
 
 import gt.edu.usac.trabajosocial.dao.DaoGeneral;
+import gt.edu.usac.trabajosocial.dominio.AsignacionRolPerfil;
 import gt.edu.usac.trabajosocial.dominio.Perfil;
 import gt.edu.usac.trabajosocial.dominio.Rol;
 import gt.edu.usac.trabajosocial.servicio.ServicioAsignacionRolPerfil;
-import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import javax.annotation.Resource;
 import org.hibernate.Query;
-import org.hibernate.criterion.DetachedCriteria;
-import org.hibernate.criterion.Restrictions;
 import org.springframework.dao.DataAccessException;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 /**
  *
  * @author Mario Batres
+ * @version 1.0
  */
 @Service
 public class ServicioAsignacionRolPerfilImpl implements  ServicioAsignacionRolPerfil{
+//______________________________________________________________________________
+    /***/
     @Resource
     private DaoGeneral daoGeneralImpl;
-
+//______________________________________________________________________________
+    
     @Override
     public List<Rol> getRolesAsignadosPorPerfil(Perfil perfil) throws DataAccessException {
         StringBuilder builder = new StringBuilder();
-        builder.append(" select rol from AsignacionRolPerfil as arf ");
-        builder.append(" inner join  arf.rol as rol ");
-        builder.append(" where arf.perfil = :perfil");
+        builder.append(" select rol from AsignacionRolPerfil as arf ")
+               .append(" inner join  arf.rol as rol ")
+               .append(" where arf.perfil = :perfil")
+               .append(" order by rol.nombre");
 
         Query query = this.daoGeneralImpl.getSesion().createQuery(builder.toString());
         query.setParameter("perfil", perfil);
         return query.list();
     }
-
+//______________________________________________________________________________
     @Override
     public List<Rol> getRolesNoAsignadosPorPerfil(Perfil perfil) throws DataAccessException {
-        List <Rol> listadoRol = this.getRolesAsignadosPorPerfil(perfil);
+        StringBuilder builder = new StringBuilder();
+        builder.append(" select rol from Rol as rol where rol.idRol not in ( ")
+               .append(" select arp.rol.idRol from AsignacionRolPerfil as arp where " )
+               .append(" arp.perfil = :perfil)")
+               .append(" order by rol.nombre");
 
-        List values = new ArrayList();
-        for(Iterator it = listadoRol.iterator(); it.hasNext();){
-            Rol rol = (Rol) it.next();
-            values.add(rol.getIdRol());
-        }
-        DetachedCriteria criteria = DetachedCriteria.forClass(Rol.class);
-        criteria.add(Restrictions.not(
-                Restrictions.in("idRol", values)
-        ));
-        return this.daoGeneralImpl.find(criteria);
+        Query query = this.daoGeneralImpl.getSesion().createQuery(builder.toString());
+        query.setParameter("perfil", perfil);
+        return query.list();
+    }
+//______________________________________________________________________________
+    @Override
+    public void agregarAsignacionRolPerfil(AsignacionRolPerfil asignacionRolPerfil)
+            throws DataIntegrityViolationException, DataAccessException {        
+        this.daoGeneralImpl.save(asignacionRolPerfil);
+    }
+//______________________________________________________________________________
+    @Override
+    public List<AsignacionRolPerfil> getAsignacionRolPerfilPorPerfil(Perfil perfil) throws DataAccessException {
+        StringBuilder builder = new StringBuilder();
+        builder.append(" select arf from AsignacionRolPerfil as arf ")
+               .append(" inner join  arf.rol as rol ")
+               .append(" where arf.perfil = :perfil")
+               .append(" order by rol.nombre");
+
+        Query query = this.daoGeneralImpl.getSesion().createQuery(builder.toString());
+        query.setParameter("perfil", perfil);
+        return query.list();
+    }
+//______________________________________________________________________________
+    @Override
+    public void eliminarAsingacionRolPerfil(int idAsignacionRolPerfil) throws DataAccessException {
+        AsignacionRolPerfil asignacionRolPerfil = this.daoGeneralImpl.load(AsignacionRolPerfil.class
+                , idAsignacionRolPerfil);
+
+        this.daoGeneralImpl.delete(asignacionRolPerfil);
     }
 
 }
