95652288ea0351507a1886da76d8b1875cf9ea4d
carlosalejand <carlosalejand@0b4acdf5-fa7f-4634-95a9-6fce243d373d>
2010-11-18 04:29:43 +0000

    Agregar clase de dominio para mantener datos de busqueda de usuario

---
+++ src/trabajo-social-orm/src/main/java/gt/edu/usac/cats/dominio/busqueda/DatosBusquedaUsuario.java
@@ -0,0 +1,115 @@
+/*
+ * Sistema de Control Academico
+ * Escuela de Trabajo Social
+ * Universidad de San Carlos de Guatemala
+ */
+
+package gt.edu.usac.cats.dominio.busqueda;
+
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+/**
+ * <p>Esta clase tiene como objetivo almacenar los datos que se ingresan en los
+ * formularios de busquedas de usuarios.</p>
+ *
+ * @author Carlos Solórzano
+ * @version 1.0
+ */
+public class DatosBusquedaUsuario {
+
+    @Pattern(regexp = "[A-Za-z0-9]*(._-)*", message = "{validacion.nombreUsuarioInvalido}")
+    @Size (max=256)
+    private String nombreUsuario;
+
+    private boolean habilitado;
+
+    /**
+     * <p>Indica si el resultado se debe de ordenar de forma ascendente (true) o
+     * descendente (false).</p>
+     */
+    private boolean ordenAscendente;
+
+    /**
+     * <p>Nombre de la columna que se usara para ordenar el listado.</p>
+     */
+    private String columnaOrden;
+
+    /**
+     * <p>Indica el primer registro que se mostrara en el listado, este permite
+     * hacer paginacion de los resultados.</p>
+     */
+    private int primerRegistro;
+
+    /**
+     * <p>Constructor de la clase, inicializa las variables.</p>
+     */
+    public DatosBusquedaUsuario(){
+        this.nombreUsuario = "";
+        this.habilitado = true;
+        this.ordenAscendente = true;
+        this.columnaOrden = "nombreUsuario";
+        this.primerRegistro = 0;
+    }
+
+//______________________________________________________________________________
+    public boolean isHabilitado() {
+        return habilitado;
+    }
+//______________________________________________________________________________
+    public void setHabilitado(boolean habilitado) {
+        this.habilitado = habilitado;
+    }
+//______________________________________________________________________________
+    public String getColumnaOrden() {
+        return columnaOrden;
+    }
+//______________________________________________________________________________
+    public void setColumnaOrden(String columnaOrden) {
+        this.columnaOrden = columnaOrden;
+    }
+//______________________________________________________________________________
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+//______________________________________________________________________________
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+//______________________________________________________________________________
+    public boolean isOrdenAscendente() {
+        return ordenAscendente;
+    }
+//______________________________________________________________________________
+    public void setOrdenAscendente(boolean ordenAscendente) {
+        this.ordenAscendente = ordenAscendente;
+    }
+//______________________________________________________________________________
+    public int getPrimerRegistro() {
+        return primerRegistro;
+    }
+//______________________________________________________________________________
+    public void setPrimerRegistro(int primerRegistro) {
+        this.primerRegistro = primerRegistro;
+    }
+//______________________________________________________________________________
+    public void aumentarPrimerRegistro() {
+//        this.primerRegistro += BotonesPaginacion.REGISTROS_MAXIMOS;
+    }
+//______________________________________________________________________________
+    public void disminuirPrimerRegistro() {
+//        this.primerRegistro -= BotonesPaginacion.REGISTROS_MAXIMOS;
+    }
+//______________________________________________________________________________
+    public void inicializarPrimerRegistro() {
+        this.primerRegistro = 0;
+    }
+//______________________________________________________________________________
+    public boolean isEmpty() {
+        if(this.nombreUsuario.isEmpty())
+            return true;
+        else
+            return false;
+    }
+
+}
