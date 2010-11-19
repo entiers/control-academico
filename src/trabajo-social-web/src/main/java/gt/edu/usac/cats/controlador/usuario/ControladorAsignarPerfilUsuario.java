3ca65775c6e36cb4db04dba443f937c8add1aa30
carlosalejand <carlosalejand@0b4acdf5-fa7f-4634-95a9-6fce243d373d>
2010-11-19 15:29:57 +0000

    Asignacion de perfiles a usuarios

---
+++ src/trabajo-social-web/src/main/java/gt/edu/usac/cats/controlador/usuario/ControladorAsignarPerfilUsuario.java
@@ -0,0 +1,157 @@
+/*
+ * Sistema de Control Academico
+ * Escuela de Trabajo Social
+ * Universidad de San Carlos de Guatemala
+ */
+
+package gt.edu.usac.cats.controlador.usuario;
+
+import org.springframework.stereotype.Controller;
+import gt.edu.usac.cats.dominio.AsignacionUsuarioPerfil;
+import gt.edu.usac.cats.dominio.Perfil;
+import gt.edu.usac.cats.dominio.Usuario;
+import gt.edu.usac.cats.servicio.ServicioUsuario;
+import gt.edu.usac.cats.servicio.ServicioGeneral;
+import gt.edu.usac.cats.util.Mensajes;
+import gt.edu.usac.cats.util.RequestUtil;
+import java.util.List;
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import org.apache.log4j.Logger;
+import org.springframework.dao.DataAccessException;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+/**
+ *
+ * @author Carlos Sol�rzano
+ * @version 1.0
+ */
+@Controller
+public class ControladorAsignarPerfilUsuario {
+
+    private static Logger log = Logger.getLogger(ControladorAsignarPerfilUsuario.class);
+//______________________________________________________________________________
+    private static final String TITULO_MENSAJE = "asignarPerfilUsuario.titulo";
+//______________________________________________________________________________
+    private Usuario usuario;
+//______________________________________________________________________________
+    @Resource
+    private ServicioGeneral servicioGeneralImpl;
+
+    @Resource
+    private ServicioUsuario servicioUsuarioImpl;
+//______________________________________________________________________________
+    /**
+     * @param modelo
+     * @param idPerfil
+     * @param request
+     *
+     * @return
+     */
+    @RequestMapping(value="asignarPerfilUsuario.htm", method = RequestMethod.GET)
+    public String crearFormulario(Model modelo, Short idUsuario, HttpServletRequest request) {
+
+        if(idUsuario == null){
+            return "usuario/buscarUsuario";
+        }
+
+        this.usuario = this.servicioGeneralImpl.cargarEntidadPorID(Usuario.class, idUsuario);
+
+        if(this.usuario == null){
+            RequestUtil.crearMensajeRespuesta(request, TITULO_MENSAJE, "buscarUsuario.sinResultados", false);
+            return "usuario/buscarUsuario";
+        }
+
+        this.setModel(modelo);
+        return "usuario/asignarPerfilUsuario";
+    }
+
+    //______________________________________________________________________________
+    /**
+     * @param modelo
+     * @param asignacionRolPerfil
+     * @param request
+     *
+     * @return
+     */
+    @RequestMapping(value="asignarPerfilUsuario.htm", method = RequestMethod.POST)
+    public String asignarRolPerfil(Model modelo, AsignacionUsuarioPerfil
+            asignacionUsuarioPerfil,  HttpServletRequest request){
+
+        try
+        {
+            asignacionUsuarioPerfil.setUsuario(this.usuario);
+            this.servicioGeneralImpl.agregar(asignacionUsuarioPerfil);
+
+            RequestUtil.crearMensajeRespuesta(request, TITULO_MENSAJE, "asignarPerfilUsuario.exito", true);
+            String msg = Mensajes.EXITO_AGREGAR + "Asignar Perfil Usuario, id = "
+                    + asignacionUsuarioPerfil.getIdAsignacionUsuarioPerfil();
+            log.info(msg);
+
+        }catch (DataAccessException e) {
+            // error de acceso a datos
+            RequestUtil.crearMensajeRespuesta(request, TITULO_MENSAJE, "dataAccessException", false);
+            log.error(Mensajes.DATA_ACCESS_EXCEPTION, e);
+        }
+
+        this.setModel(modelo);
+        return "usuario/asignarPerfilUsuario";
+    }
+    /**
+     * @param modelo
+     * @param idAsignacionRolPerfil
+     * @param request
+     *
+     * @return
+     */
+//______________________________________________________________________________
+    @RequestMapping(value="desasignarUsuarioPerfil", method = RequestMethod.GET)
+    public String desasignarRolPerfil(Model modelo, Integer idAsignacionUsuarioPerfil
+            , HttpServletRequest request){
+
+        if(idAsignacionUsuarioPerfil != null){
+            try{
+                AsignacionUsuarioPerfil asignacionUsuarioPerfil = this.servicioGeneralImpl
+                        .cargarEntidadPorID(AsignacionUsuarioPerfil.class, idAsignacionUsuarioPerfil);
+
+                this.servicioGeneralImpl.borrar(asignacionUsuarioPerfil);
+                RequestUtil.crearMensajeRespuesta(request, TITULO_MENSAJE, "desasignarUsuarioPerfil.exito", true);
+                String msg = Mensajes.EXITO_AGREGAR + "Desasignar Rol Perfil, id = "+ idAsignacionUsuarioPerfil;
+                log.info(msg);
+            }catch(DataAccessException e){
+                RequestUtil.crearMensajeRespuesta(request, TITULO_MENSAJE, "dataAccessException", false);
+                log.error(Mensajes.DATA_ACCESS_EXCEPTION, e);
+            }
+        }
+
+        this.setModel(modelo);
+        return "usuario/asignarPerfilUsuario";
+    }
+
+
+    //______________________________________________________________________________
+    /**
+     * @param modelo
+     **/
+    private void setModel(Model modelo){
+        List <AsignacionUsuarioPerfil> listadoAsignacionPerfilUsuario = this.servicioUsuarioImpl
+                .getAsignacionUsuarioPerfilPorUsuario(usuario);
+                
+
+        List <Perfil> listadoPerfilNoAsignado = this.servicioUsuarioImpl
+                .getPerfilesNoAsignadosPorUsuario(usuario);
+
+        // se agregan los objetos que se usaran en la pagina
+        modelo.addAttribute("usuario", this.usuario);
+
+        modelo.addAttribute("asignacionUsuarioPerfil", new AsignacionUsuarioPerfil());
+        modelo.addAttribute("listadoAsignacionPerfilUsuario", listadoAsignacionPerfilUsuario);
+        modelo.addAttribute("listadoPerfilNoAsignado", listadoPerfilNoAsignado);
+
+    }
+
+
+}
--- src/trabajo-social-web/src/main/webapp/vistas/jsp/usuario/asignarPerfilUsuario.jsp
+++ src/trabajo-social-web/src/main/webapp/vistas/jsp/usuario/asignarPerfilUsuario.jsp
@@ -6,14 +6,63 @@
 
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
-   "http://www.w3.org/TR/html4/loose.dtd">
-
+    "http://www.w3.org/TR/html4/loose.dtd">
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt" %>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>JSP Page</title>
+        <title><fmt:message key="asignarPerfilUsuario.titulo"/></title>
+        <%@include file="../../jspf/scripts/scriptUsuario.jspf" %>
     </head>
     <body>
-        <h1>Hello World!</h1>
+        <h1><fmt:message key="asignarPerfilUsuario.titulo" /></h1>
+
+        <form:form modelAttribute="asignacionUsuarioPerfil" method="post" >
+        <fieldset>
+            <legend><fmt:message key="usuario.perfilesNoAsignados"/></legend>
+
+            <div id="divCampos">
+                <label for="usuario"><fmt:message key="asignarPerfilUsuario.usuario"/>: </label>
+                <span>${usuario.nombreUsuario}</span>
+            </div>
+
+            <div id="divCampos">
+                <label for="perfil"><fmt:message key="asignarPerfilUsuario.perfil"/>: </label>
+                <form:select itemLabel="nombre"path="perfil.idPerfil" itemValue="idPerfil"
+                             items="${listadoPerfilNoAsignado}" />
+                <input type="submit" value='<fmt:message key="btnAgregar"/>' />
+            </div>
+        </fieldset>
+    </form:form>
+
+     <fieldset>
+            <legend><fmt:message key="usuario.perfilesAsignados"/></legend>
+            <table>
+                <thead>
+                    <tr>
+                        <th><fmt:message key="Perfil.menu"/></th>
+                        <th><fmt:message key="acciones"/></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <c:forEach items="${listadoAsignacionPerfilUsuario}" var="asignacionPerfilUsuario">
+                        <tr>
+                            <td>${asignacionPerfilUsuario.perfil.nombre}</td>
+                            <td>
+                                <a href="desasignarUsuarioPerfil.htm?idAsignacionUsuarioPerfil=${asignacionPerfilUsuario.idAsignacionUsuarioPerfil}">
+                                    <fmt:message key="asignarPerfilUsuario.eliminar"/>
+                                </a>
+                            </td>
+                        </tr>
+                    </c:forEach>
+                </tbody>
+            </table>
+        </fieldset>
+        
+        <%-- fragmento que muestra como mensaje popup el resultado de las operaciones --%>
+        <%@include file="../../jspf/plantilla/popupMensaje.jspf" %>
+        
     </body>
 </html>
