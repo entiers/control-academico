859a091de600beae85170499cae7fc2e91ba3169
shakamca <shakamca@0b4acdf5-fa7f-4634-95a9-6fce243d373d>
2010-12-10 22:20:27 +0000

    Agregando documentación interna al proyecto de ETL.

--- trunk/src/trabajo-social-etl/src/main/java/gt/edu/usac/cats/etl/FabricaManejadorETL.java
+++ trunk/src/trabajo-social-etl/src/main/java/gt/edu/usac/cats/etl/FabricaManejadorETL.java
@@ -1,18 +1,29 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Sistema de Control Academico
+ * Escuela de Trabajo Social
+ * Universidad de San Carlos de Guatemala
  */
 
 package gt.edu.usac.cats.etl;
 
 /**
+ * Clase enum fábrica que se encarga de crear las instancias de los objetos que
+ * manejan las llamadas a los distintos ETL que hay.
  *
  * @author Mario Batres
  * @version 1.0
  */
 public enum FabricaManejadorETL{
+//______________________________________________________________________________
+    /** Maneja el tipo para el ETL de RegistroCSV  */
     REGISTRO_CSV;
-
+//______________________________________________________________________________
+    /**
+     * Método que se encarga de crear la instancia de acuerdo al tipo enum de
+     * donde es llamado.
+     *
+     * @return Instancia de tipo {@link ManejadorETL}     *
+     */
     public ManejadorETL crear(){
         switch(this){
             case REGISTRO_CSV:
@@ -20,4 +31,5 @@ public enum FabricaManejadorETL{
         }
         return null;
     }
+//______________________________________________________________________________
 }
--- trunk/src/trabajo-social-etl/src/main/java/gt/edu/usac/cats/etl/ManejadorETL.java
+++ trunk/src/trabajo-social-etl/src/main/java/gt/edu/usac/cats/etl/ManejadorETL.java
@@ -9,6 +9,11 @@ import java.io.IOException;
 import java.net.ServerSocket;
 
 /**
+ * Clase abstracta que lleva maneja el comportamiento para el llamado a las
+ * clases que realizan los procesos de ETL.
+ * <br/><br/>
+ * Se utiliza el puerto 3334 para la apertura de un socket que es utilizado
+ * por los diferentes procesos de ETL
  *
  * @author Mario Batres
  * @version 1.0
@@ -16,20 +21,27 @@ import java.net.ServerSocket;
 public abstract class ManejadorETL {  
 
 //______________________________________________________________________________
+    /**
+     * Método abstracto que obliga a implementarlo en las clases hijas.  Su función
+     * prinicipal es llamar a las clases de los diferentes procesos ETL que existen.
+     *
+     * @return Arreglo bidimensional de cadenas que retornan lo ocurrido en la
+     * realización del proceso.
+     * 
+     * @throws IOException
+     */
     protected abstract String [][] realizarETL();
 //______________________________________________________________________________
-    /*private void abrirSocket() throws IOException{
-        if (serverSocket == null || serverSocket.isClosed()){
-            serverSocket = new ServerSocket(3334);
-        }
-    }
-//______________________________________________________________________________
-    private void cerrarSocket(){
-        if (serverSocket != null && !serverSocket.isClosed()){
-            serverSocket.isClosed();
-        }
-    }*/
-//______________________________________________________________________________
+    /**
+     * Método que se encarga de la apertura del socket para realizar el ETL Y se
+     * hace el llamado al método <b>realizarETL</b> que es implementado en las clases
+     * hijas.
+     *
+     * @return De tipo <b>String [][]</b>. Arreglo bidimensional de cadenas que
+     * retornan lo ocurrido en la realización del proceso.
+     *
+     * @throws IOException
+     */
     public String [][] realizar() throws IOException{
         ServerSocket serverSocket = new ServerSocket(3334);        
         String [][]ret = this.realizarETL();
@@ -37,5 +49,4 @@ public abstract class ManejadorETL {
         return ret;
     }
 //______________________________________________________________________________
-
 }
