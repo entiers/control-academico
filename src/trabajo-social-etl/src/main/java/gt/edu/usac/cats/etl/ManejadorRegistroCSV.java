0e9caf221cbffd5347bba885d2a0fa5ab9cda941
shakamca <shakamca@0b4acdf5-fa7f-4634-95a9-6fce243d373d>
2010-12-10 22:20:27 +0000

    Agregando documentación interna al proyecto de ETL.

--- src/trabajo-social-etl/src/main/java/gt/edu/usac/cats/etl/ManejadorRegistroCSV.java
+++ src/trabajo-social-etl/src/main/java/gt/edu/usac/cats/etl/ManejadorRegistroCSV.java
@@ -8,12 +8,26 @@ package gt.edu.usac.cats.etl;
 import registrocsv.etlregistrocsv_0_1.ETLRegistroCSV;
 
 /**
+ * Clase que hereda el comportamiento de {@link ManejadorETL}, esta clase lleva
+ * el control del llamado el módulo {@link ETLRegistroCSV} que realiza el ETL
+ * del archivo CSV que proporciona Registro y Estadística con el control de los
+ * estudiantes incritos (nuevos y de reingreso).
  *
  * @author Mario Batres
  * @version 1.0
+ * @see ManejadorETL
  */
 public class ManejadorRegistroCSV extends ManejadorETL {
 
+    /**
+     * Método que se debe de sobreescribir para indicar qué módulo de ETL se
+     * estará usando.
+     *
+     * @return Arreglo bidimensional de cadenas que retornan lo ocurrido en la
+     * realización del proceso.
+     * 
+     * @throws IOException
+     */
     @Override
     protected String[][] realizarETL() {
         ETLRegistroCSV etl = new ETLRegistroCSV();
