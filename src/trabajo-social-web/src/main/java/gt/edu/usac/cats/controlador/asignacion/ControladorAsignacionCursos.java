f322e4c7f583593e25a1905a1e07b6aac52386e2
carlosalejand <carlosalejand@0b4acdf5-fa7f-4634-95a9-6fce243d373d>
2013-03-26 17:22:32 +0000

    - Resolviendo bug de asignaciones "fantasma"

--- src/trabajo-social-web/src/main/java/gt/edu/usac/cats/controlador/asignacion/ControladorAsignacionCursos.java
+++ src/trabajo-social-web/src/main/java/gt/edu/usac/cats/controlador/asignacion/ControladorAsignacionCursos.java
@@ -26,6 +26,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import org.apache.log4j.Logger;
 import org.codehaus.jackson.annotate.JsonIgnore;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.dao.DataAccessException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -43,6 +45,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
  * @version 1.0
  */
 @Controller("controladorAsignacionCursos")
+@Scope(value="session", proxyMode=ScopedProxyMode.TARGET_CLASS)
 public class ControladorAsignacionCursos extends ControladorAbstractoAsignacion{
 //______________________________________________________________________________
     private static Logger log = Logger.getLogger(ControladorAsignacionCursos.class);
