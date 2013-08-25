1b130f3e6d99495a9968c8e0dcf181b069854ae7
shakamca <shakamca@0b4acdf5-fa7f-4634-95a9-6fce243d373d>
2013-08-25 01:18:13 +0000

    Arreglando bugs desconocidos.

--- trunk/src/trabajo-social-web/src/main/java/gt/edu/usac/cats/controlador/pensumEstudianteCarrera/ControladorEliminarPensumEstudianteCarrera.java
+++ trunk/src/trabajo-social-web/src/main/java/gt/edu/usac/cats/controlador/pensumEstudianteCarrera/ControladorEliminarPensumEstudianteCarrera.java
@@ -3,13 +3,15 @@
  * Escuela de Trabajo Social
  * Universidad de San Carlos de Guatemala
  */
-
 package gt.edu.usac.cats.controlador.pensumEstudianteCarrera;
 
+import gt.edu.usac.cats.dominio.Pensum;
 import gt.edu.usac.cats.dominio.PensumEstudianteCarrera;
+import gt.edu.usac.cats.dominio.wrapper.WrapperEquivalenciaPorPensum;
 import gt.edu.usac.cats.util.Mensajes;
 import gt.edu.usac.cats.util.RequestUtil;
 import java.io.Serializable;
+import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import org.apache.log4j.Logger;
 import org.springframework.context.annotation.Scope;
@@ -22,9 +24,10 @@ import org.springframework.web.context.WebApplicationContext;
 
 /**
  * <p>Controlador que se encarga de eliminar una asignaci&oacute;n de un pensum
- * con el estudiante y su carrera. Objeto de tipo {@link PensumEstudianteCarrera}</p>
- * 
- * <p>Se utiliza en en la p&aacute;gina 
+ * con el estudiante y su carrera. Objeto de tipo
+ * {@link PensumEstudianteCarrera}</p>
+ *
+ * <p>Se utiliza en en la p&aacute;gina
  * <code>eliminarPensumEstudianteCarrera.htm</code> </p>
  *
  * @author Mario Batres
@@ -32,7 +35,7 @@ import org.springframework.web.context.WebApplicationContext;
  */
 @Controller
 @Scope(value = WebApplicationContext.SCOPE_SESSION)
-public class ControladorEliminarPensumEstudianteCarrera extends ControladorAbstractoPensumEstudianteCarrera implements Serializable{
+public class ControladorEliminarPensumEstudianteCarrera extends ControladorAbstractoPensumEstudianteCarrera implements Serializable {
 
 //______________________________________________________________________________
     /**
@@ -42,12 +45,13 @@ public class ControladorEliminarPensumEstudianteCarrera extends ControladorAbstr
 
 //______________________________________________________________________________
     /**
-     * Este m&eacute;todo elimina la asignaci&oacute;n de un pensum con el estudiante
-     * y su carrera en la base de datos.
+     * Este m&eacute;todo elimina la asignaci&oacute;n de un pensum con el
+     * estudiante y su carrera en la base de datos.
      *
      * @param modelo Objeto de tipo {@link Model}
-     * @param idPensumEstudianteCarrera Objeto de tipo {@link Integer} que obtiene el id de
-     * la asignaci&oacute;n del pensum con el estudinate y la carrera.
+     * @param idPensumEstudianteCarrera Objeto de tipo {@link Integer} que
+     * obtiene el id de la asignaci&oacute;n del pensum con el estudinate y la
+     * carrera.
      * @param request Objeto de tipo {@link HttpServletRequest}
      *
      * @return String de la vista a mostrar
@@ -85,7 +89,37 @@ public class ControladorEliminarPensumEstudianteCarrera extends ControladorAbstr
             log.error(Mensajes.DATA_ACCESS_EXCEPTION, e);
         }
 
-        modelo.addAttribute("pensumEstudianteCarrera", null);
+        
+        //__________
+
+        PensumEstudianteCarrera pensumEstudianteCarreraValido = this.servicioPensumEstudianteCarreraImpl.
+                getPensumEstudianteCarreraValido(pensumEstudianteCarrera.getAsignacionEstudianteCarrera());
+
+        Pensum pensum = null;
+        WrapperEquivalenciaPorPensum wrapperEquivalenciaPorPensum = null;
+
+        //Se obtiene las relaciones con el pensum valido
+        if (pensumEstudianteCarreraValido != null) {
+            pensum = pensumEstudianteCarreraValido.getPensum();
+
+            modelo.addAttribute("idPensumEstudianteCarreraValido",
+                    pensumEstudianteCarreraValido.getIdPensumEstudianteCarrera());
+
+            wrapperEquivalenciaPorPensum =
+                    new WrapperEquivalenciaPorPensum(pensumEstudianteCarrera.getAsignacionEstudianteCarrera(), pensum);
+
+        } else {
+            wrapperEquivalenciaPorPensum = new WrapperEquivalenciaPorPensum();
+        }
+        request.setAttribute("aec", pensumEstudianteCarrera.getAsignacionEstudianteCarrera());
+
+        List<PensumEstudianteCarrera> listadoPensumEstudianteCarreraNoValidos = null;
+        List<Pensum> listadoPensumsNoAsignadosAEsutudianteCarrera = null;
+        //this.agregarAtributosDefault(modelo, new PensumEstudianteCarrera(), false, true);
+        this.agregarAtributosDefault(modelo, pensum, pensumEstudianteCarrera.getAsignacionEstudianteCarrera(), pensumEstudianteCarreraValido,
+                listadoPensumEstudianteCarreraNoValidos, listadoPensumsNoAsignadosAEsutudianteCarrera,
+                wrapperEquivalenciaPorPensum, false, true);
+
         return "pensumEstudianteCarrera/mostrarPensumEstudianteCarrera";
     }
 }
